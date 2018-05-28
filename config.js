9d461e6d7847f91ac2222dafb551a452b3c76dee
Shaikh Mohasin <mohasin69@gmail.com>
2018-05-28 10:40:15 +0530

    syntac changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const token = process.env.EG_TRANS_TOKEN;
+const token = '"'+process.env.EG_TRANS_TOKEN+'"';
 const prefix = "!eg";
 const ownner = ['"'+process.env.OWNER1+'"'];
-const playingStatus = process.env.PLAYING_STATUS;
+const playingStatus = '"'+ process.env.PLAYING_STATUS+'"';
 const tsChannelsEnabled = true;
 
